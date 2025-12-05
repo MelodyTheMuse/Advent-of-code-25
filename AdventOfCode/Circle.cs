7e86f2017560a39639bbc5b3717e1f23289a16f0
MelodyTheMuse <105441849+MelodyTheMuse@users.noreply.github.com>
2025-12-05 23:36:08 +0100

    Day 1 part 1 done

--- AdventOfCode/Circle.cs
+++ AdventOfCode/Circle.cs
@@ -4,10 +4,14 @@ namespace AdventOfCode;
 
 public class Circle
 {
-    private int CurrentNumber = 0;
+    public static Circle _Instance;
+    private int CurrentNumber = 50;
     private int maxNumber = 99;
     private int minNumber = 0;
     private int clicks = 0;
+    private int steps = 0;
+    private bool isNewNumber = false;
+    public static int clickers;
 
 
     private bool direction(string input)
@@ -19,38 +23,133 @@ public class Circle
         return false;
     }
 
+    private static void Main()
+    {
+      DoRun();
+      
+    }
+
+    public static void DoRun()
+    { 
+        clickers  = GetInstance().Run();
+    }
+
+    public static Circle GetInstance()
+    {
+        if (_Instance == null)
+            _Instance = new Circle();
+        return _Instance;
+    }
+
+    public int Run()
+    {
+        foreach (var item in input)
+        {
+            isNewNumber = true;
+            GetClick(item);
+            steps++;
+            Console.WriteLine(clicks + " clicks");
+            Console.WriteLine(steps + " steps");
+            Console.WriteLine(CurrentNumber + " current number");
+        }
+
+        return clicks;
+
+    }
+    
+    public int RunFromParameter(List<string> input)
+    {
+        foreach (var item in input)
+        {
+            isNewNumber = true;
+            GetClick(item);
+            steps++;
+            Console.WriteLine(clicks + " clicks");
+            Console.WriteLine(steps + " steps");
+            Console.WriteLine(CurrentNumber + " current number");
+        }
+
+        return clicks;
+
+    }
+
     private void GetClick(string input)
     {
         var output = splitStringInt(input);
         CurrentNumber = CalculateNewNumber(currentNumber: CurrentNumber, input: output, direction: direction(input));
-        Console.WriteLine(clicks);
+        if (CurrentNumber == 0)
+        {
+            clicks++;
+        }
     }
-    private int CalculateNewNumber(int currentNumber, int input, bool direction)
+    public int CalculateNewNumber(int currentNumber, int input, bool direction,bool isnewNumber = true)
     {
         int difference;
         if (direction)
         {
-            if (currentNumber - input < minNumber)
-            {
-                difference =  input - currentNumber;
-                CalculateNewNumber(maxNumber, difference, direction);
+            if ((currentNumber - input) < minNumber)
+            {  
+                difference =  input - (currentNumber + 1);
+                var newNumber = CalculateNewNumber(maxNumber, difference, direction,false);
+                newNumber = checkNewNumber(newNumber, direction);
+               // clicks++;
+               if (newNumber != minNumber && currentNumber != minNumber)clicks++;
+                return newNumber;
             }
 
-            if (currentNumber - input != minNumber) return currentNumber - input;
-            clicks++;
+            if (currentNumber - input != minNumber ) return currentNumber - input;
+            if (currentNumber == minNumber && !isNewNumber)
+            {
+               // clicks++;
+                return minNumber;
+            }
+           // clicks++;
             return minNumber;
         }
-        if (currentNumber +input > maxNumber)
+        if ((currentNumber + input) > maxNumber)
         {
-            difference = input - currentNumber;
-            CalculateNewNumber(minNumber, difference, direction);
+           
+            difference = input - (maxNumber - currentNumber) -1 ;
+            var newNumber = CalculateNewNumber(minNumber, difference, direction,false);
+            newNumber = checkNewNumber(newNumber, direction);
+            if (newNumber != minNumber && currentNumber != minNumber)clicks++;
+            return newNumber;
         }
 
-        if (currentNumber - input != minNumber) return currentNumber + input;
-        clicks++;
-        return minNumber;
+        if (currentNumber + input != maxNumber) return currentNumber + input;
+        if(currentNumber == maxNumber && !isNewNumber){ return maxNumber;}
+        //clicks++;
+        return maxNumber;
        
     }
     
     private int splitStringInt(string input) => int.Parse(input.Substring(1));
+    
+    
+    private List<string> input = File.ReadAllLines("S:\\Repo\\GODOT\\contamination-v-2\\Advent-of-code-25\\input.txt").ToList();
+
+    public int checkNewNumber(int newNumber, bool direction)
+    { int difference;
+        if (newNumber == minNumber )
+        {
+           // clicks++; 
+            return newNumber;
+        }
+        if (newNumber != maxNumber && newNumber < maxNumber && newNumber > minNumber && newNumber != minNumber ) return newNumber;
+        if (newNumber > maxNumber)
+        {
+            difference = newNumber - maxNumber;
+            //clicks++;  
+            return CalculateNewNumber(minNumber, difference, direction,false);
+        }
+
+        if (newNumber < minNumber)
+        {
+            difference = minNumber - newNumber;
+            //clicks++; 
+            return CalculateNewNumber(maxNumber, difference, direction,false);
+        }
+        
+        return newNumber;
+    }
 }
---
+++ TestingAdventOfCode/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode;
+
+namespace TestingAdventOfCode;
+
+public class CircleTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    [Test]
+    public void checkNewNumber()
+    {
+        Circle circle = new Circle();
+        Assert.That(circle.checkNewNumber((-12), true), Is.EqualTo(87));
+    }
+
+
+    [Test]
+    public void testCalcNewNumber()
+    {
+        Circle circle = new Circle();
+        Assert.That(circle.CalculateNewNumber(0, 99, true), Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void testRunWParameter() 
+    {
+        List<string> input = File.ReadAllLines("S:\\Repo\\GODOT\\contamination-v-2\\Advent-of-code-25\\test_input.txt").ToList(); 
+        Circle circle = new Circle();
+        Assert.That(circle.RunFromParameter(input), Is.EqualTo(6));
+    }
+    
+    [Test]
+    public void testRun() 
+    {
+        Circle circle = new Circle();
+        Assert.That(circle.Run(), Is.EqualTo(3961));
+    }
+}
